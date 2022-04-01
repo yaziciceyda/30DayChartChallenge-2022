736dc1863bab34f4a52d1b273e78f2f30084e22e
yaziciceyda <79111722+yaziciceyda@users.noreply.github.com>
2022-04-01 23:01:29 +0300

    Add files via upload

---
+++ Day 1/Day 1.R
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(showtext)
+library(ggtext)
+
+
+youth_data <- read_xlsx("Data_Day1.xlsx", sheet = "Data_Youth")
+
+youth_data <- youth_data %>%
+  mutate(Percent_non = 100 - Percent) %>%
+  # filter(Country %in% c("Croatia",
+  #                      "Turkey",
+  #                      "Netherlands",
+  #                      "Denmark")) %>%
+  pivot_longer(!Country, names_to = "Youth_Level", values_to = "Percent")
+
+font_add_google("DM Serif Display", "sd")
+showtext_auto()
+
+hsize = 2
+p <- ggplot(youth_data, aes(x = hsize, y = Percent, color = Youth_Level,fill = Youth_Level)) +
+  coord_polar(theta = "y") +
+  xlim(c(0.2, hsize + 0.5))+
+  facet_wrap(~ Country) +
+geom_col(color = "white")+
+  scale_fill_manual(values = c("#E072A7", "#EE9D1A"),
+                    labels = c("Living with their parents",
+                     "Not living with their parents"))+
+  geom_text(aes(label = paste0(Percent,"%")), size = 5,
+            colour="black", family = "sd",
+            position = position_stack(vjust = 0.5)) +
+  labs(title = "Percentage of Young Adults aged 18-34 living with their Parents",
+       subtitle = "<span style='color:#553746'>Croatia has the highest percent of <span style= 'color:#E072A7'> young adults living with their parents,<br><span style='color:#553746'> while Denmark
+       has the highest percentage of them <span style='color:#EE9D1A'> not living with their parents.<span style='color:#C5FD05'> </span>",
+       caption = "Data: Eurostat |#30DayChartChallenge - Day #1 |Prepared by: @Cyd_yzc") +
+  theme(
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.ticks = element_blank(),
+    axis.line = element_blank(), 
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    panel.grid = element_blank(),
+    plot.margin = unit(c(0.1, 0.2, 0.1, 0.2),"cm"),
+    plot.background = element_rect(fill = "#B2E1DB"),
+    panel.background = element_rect(fill = "#B2E1DB"),
+    strip.background = element_rect(fill = "#B2E1DB"),
+    strip.text = element_text(size = 17, family = "sd"),
+    legend.background = element_rect(fill = "#B2E1DB"),
+    legend.title = element_blank(),
+    plot.subtitle = element_markdown(family = "sd", size = 23, margin = margin(b = 30)),
+    legend.text = element_text(family = "sd", size = 15, hjust = 0),
+    plot.title = element_text(family = "sd", size = 30),
+    plot.caption = element_text(family = "sd", hjust = 1, size = 18)
+    
+  )
+ggsave("Day1.png",p, width = 13, height = 18, units = "cm")  
